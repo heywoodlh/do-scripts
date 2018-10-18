0f7772c2f9ac185968138ef6ef1d339d6254b00f
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-10-18 13:30:26 -0600

    destroy-droplet.sh prints selection before destroying droplet

--- destroy-droplet.sh
+++ destroy-droplet.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 clear
+DROPLET_LIST="$(doctl compute droplet list)"
 echo 'Current droplets: '
-doctl compute droplet list
+echo "$DROPLET_LIST"
 
 printf "\n"
 echo 'Droplet ID or name: '
 read DROPLET_ID
+
+clear
+printf "\n"
+echo 'Selection: '
+echo "$DROPLET_LIST" | grep "$DROPLET_ID"
+
+printf "\n"
 doctl compute droplet delete "$DROPLET_ID"
