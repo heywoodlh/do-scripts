c16417cb41251e03de64be9d9d22247708e68fe4
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-10-18 16:15:54 -0600

    added poweron/off scripts

---
+++ poweroff-droplet.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+clear
+DROPLET_LIST="$(doctl compute droplet list)"
+echo 'Current droplets: '
+echo "$DROPLET_LIST"
+
+printf "\n"
+echo 'Droplet ID or name: '
+read DROPLET_ID
+
+clear
+printf "\n"
+echo 'Selection: '
+echo "$DROPLET_LIST" | grep "$DROPLET_ID"
+
+printf "\n"
+doctl compute droplet-action power-off "$DROPLET_ID"
